439bcbd086bc3b9c9dcb4fffa478e89e2fa6d595
Xiaozhihong <18673960926@163.com>
2019-01-15 20:30:33 +0800

    new

--- main.py
+++ main.py
@@ -3,18 +3,5 @@
 import tensorflow as tf
 import numpy as np
 
-
-def valid_scale_filter(bboxes, valid_scale):
-    print bboxes
-    bboxes_scale = np.sqrt(np.multiply.reduce(bboxes[:, 2:4] - bboxes[:, 0:2], axis=-1))
-    print bboxes_scale
-    scale_mask = np.logical_and((valid_scale[0] < bboxes_scale), (bboxes_scale < valid_scale[1]))
-    print scale_mask
-    bboxes = bboxes[scale_mask]
-    return bboxes
-
 if __name__ == '__main__':
-    bboxes = np.array([[0, 0, 29, 29], [10, 10, 35, 45]])
-    valid_scale = np.array([0, 30])
-    bboxes = valid_scale_filter(bboxes, valid_scale)
-    print bboxes
+    pass
